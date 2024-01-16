f3d38ee92b30fd24ee8e5b6a6bc909dfa331a326
Tristan Schwartz <tristan@lesentrecodeurs.com>
2024-01-16 08:56:22 +0100

    refactor: crud folder

---
+++ packages/api-core/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./helpers";
+export * from "./lib/files";
+export * from "./lib/multer";
+export * from "./lib/service.config";
+export * from "./middlewares";
+export * from "./types";
+export * from "./crud";
