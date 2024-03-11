7c11198561f20e5c1a4d5cab0c0005331f43ac0f
Haorsis <cadetgauthier3@gmail.com>
2024-03-11 16:56:42 +0100

    admin gateway swagger

--- apps/admin-gateway/src/features/categories/categories.controller.ts
+++ apps/admin-gateway/src/features/categories/categories.controller.ts
@@ -8,12 +8,28 @@ class CategoriesCtrl extends GatewayController {
     });
   }
 
+  /**
+   * @swagger
+   * /categories:
+   *   get:
+   *     summary: Retrieve a list of Categories
+   *     tags: 
+   *       - Category
+   */
   getListCategories: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("getListCategories"), res, async () => {
       const { data } = await this.fetcher.get("/categories");
       return data;
     });
 
+  /**
+   * @swagger
+   * /categories/{id}:
+   *   get:
+   *     summary: Retrieve Category from given ID
+   *     tags: 
+   *       - Category
+   */
   getOneCategory: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("getOneCategory"), res, async () => {
       const { id } = req.params;
@@ -24,12 +40,51 @@ class CategoriesCtrl extends GatewayController {
       return data;
     });
 
+  /**
+   * @swagger
+   * /categories:
+   *   post:
+   *     summary: Create Category
+   *     tags: 
+   *       - Category
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               name:
+   *                 type: string
+   *               company_id:
+   *                 type: integer
+   */
   createCategory: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("createCategory"), res, async () => {
       const { data } = await this.fetcher.post("/categories", req.body);
       return data;
     });
 
+  /**
+   * @swagger
+   * /categories/{id}:
+   *   patch:
+   *     summary: Update Category from given ID with body
+   *     description: send JSON with only fields to edit
+   *     tags: 
+   *       - Category
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               name:
+   *                 type: string
+   *               company_id:
+   *                 type: integer
+   */
   updateCategory: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("updateCategory"), res, async () => {
       const { id } = req.params;
@@ -40,6 +95,14 @@ class CategoriesCtrl extends GatewayController {
       return data;
     });
 
+  /**
+   * @swagger
+   * /categories/{id}:
+   *   delete:
+   *     summary: Delete Category from given ID
+   *     tags: 
+   *       - Category
+   */
   deleteCategory: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("deleteCategory"), res, async () => {
       const { id } = req.params;
--- apps/admin-gateway/src/features/products/products.controller.ts
+++ apps/admin-gateway/src/features/products/products.controller.ts
@@ -8,12 +8,28 @@ class ProductsCtrl extends GatewayController {
     });
   }
 
+  /**
+   * @swagger
+   * /products:
+   *   get:
+   *     summary: Retrieve a list of Products
+   *     tags:
+   *       - Product
+   */
   getlistProducts: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("getlistProducts"), res, async () => {
       const { data } = await this.fetcher.get("/products");
       return data;
     });
 
+  /**
+   * @swagger
+   * /products/{id}:
+   *   get:
+   *     summary: Retrieve Product from given ID
+   *     tags:
+   *       - Product
+   */
   getOneProduct: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("getOneProduct"), res, async () => {
       const { id } = req.params;
@@ -24,12 +40,83 @@ class ProductsCtrl extends GatewayController {
       return data;
     });
 
+  /**
+   * @swagger
+   * /products:
+   *   post:
+   *     summary: Create Product
+   *     tags:
+   *       - Product
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               name:
+   *                 type: string
+   *               price:
+   *                 type: integer
+   *               description:
+   *                 type: string
+   *               photo:
+   *                 type: string
+   *               conditioning:
+   *                 type: string
+   *               size:
+   *                 type: string
+   *               stock:
+   *                 type: integer
+   *               unity:
+   *                 type: string
+   *               category_id:
+   *                 type: integer
+   *               parent_id:
+   *                 type: integer
+   */
   createProduct: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("createProduct"), res, async () => {
       const { data } = await this.fetcher.post("/products", req.body);
       return data;
     });
 
+  /**
+   * @swagger
+   * /products/{id}:
+   *   patch:
+   *     summary: Update Product from given ID with body
+   *     description: send JSON with only fields to edit
+   *     tags:
+   *       - Product
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               name:
+   *                 type: string
+   *               price:
+   *                 type: integer
+   *               description:
+   *                 type: string
+   *               photo:
+   *                 type: string
+   *               conditioning:
+   *                 type: string
+   *               size:
+   *                 type: string
+   *               stock:
+   *                 type: integer
+   *               unity:
+   *                 type: string
+   *               category_id:
+   *                 type: integer
+   *               parent_id:
+   *                 type: integer
+   */
   updateProduct: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("updateProduct"), res, async () => {
       const { id } = req.params;
@@ -40,6 +127,14 @@ class ProductsCtrl extends GatewayController {
       return data;
     });
 
+  /**
+   * @swagger
+   * /products/{id}:
+   *   delete:
+   *     summary: Delete Product from given ID
+   *     tags:
+   *       - Product
+   */
   deleteProduct: RequestHandler = (req, res) =>
     ctrlWrapper(this.getIdentifier("deleteProduct"), res, async () => {
       const { id } = req.params;
