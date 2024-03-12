59a8a2eb576e41bbd14c0b6384393e56be35f9cf
Haorsis <cadetgauthier3@gmail.com>
2024-03-12 14:26:17 +0100

    add isLogged middleware to company and company-customer routes in admin gateway

--- apps/admin-gateway/src/features/companies/companies.router.ts
+++ apps/admin-gateway/src/features/companies/companies.router.ts
@@ -3,10 +3,10 @@ import { companiesCtrl } from "./companies.controller";
 import { isLogged } from "@ce/server-core";
 
 const companiesRouter = express.Router();
-companiesRouter.get("/", [], companiesCtrl.getlistCompanies);
-companiesRouter.get("/:id", [], companiesCtrl.getOneCompany);
+companiesRouter.get("/", [isLogged], companiesCtrl.getlistCompanies);
+companiesRouter.get("/:id", [isLogged], companiesCtrl.getOneCompany);
 companiesRouter.post("/", [], companiesCtrl.createCompany);
-companiesRouter.patch("/:id", [], companiesCtrl.updateCompany);
-companiesRouter.delete("/:id", [], companiesCtrl.deleteCompany);
+companiesRouter.patch("/:id", [isLogged], companiesCtrl.updateCompany);
+companiesRouter.delete("/:id", [isLogged], companiesCtrl.deleteCompany);
 
 export { companiesRouter };
--- apps/admin-gateway/src/features/company-customers/company-customers.router.ts
+++ apps/admin-gateway/src/features/company-customers/company-customers.router.ts
@@ -1,30 +1,31 @@
 import express from "express";
 import { companyCustomersCtrl } from "./company-customers.controller";
+import { isLogged } from "@ce/server-core";
 
 const companyCustomersRouter = express.Router();
 companyCustomersRouter.get(
   "/",
-  [],
+  [isLogged],
   companyCustomersCtrl.getlistCompanyCustomers
 );
 companyCustomersRouter.get(
   "/:id",
-  [],
+  [isLogged],
   companyCustomersCtrl.getOneCompanyCustomer
 );
 companyCustomersRouter.post(
   "/",
-  [],
+  [isLogged],
   companyCustomersCtrl.createCompanyCustomer
 );
 companyCustomersRouter.patch(
   "/:id",
-  [],
+  [isLogged],
   companyCustomersCtrl.updateCompanyCustomer
 );
 companyCustomersRouter.delete(
   "/:id",
-  [],
+  [isLogged],
   companyCustomersCtrl.deleteCompanyCustomer
 );
 
