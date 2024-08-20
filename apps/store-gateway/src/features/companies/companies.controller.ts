8a973798ac13fd83d010a26f41f834c960b3decf
SophieH57 <marinsophie91@yahoo.fr>
2024-08-20 22:41:05 +0200

    feat: company-customer

--- apps/store-gateway/src/features/companies/companies.controller.ts
+++ apps/store-gateway/src/features/companies/companies.controller.ts
@@ -1,8 +1,34 @@
-import { GatewayController } from "@ce/server-core";
-import { CompaniesService } from "./companies.service";
+import {
+  BaseResponse,
+  GatewayController,
+  HttpException,
+  ctrlWrapper,
+  parseBody
+} from '@ce/server-core';
+import { CompaniesService } from './companies.service';
+import { RequestHandler } from 'express';
 
 export class CompaniesController extends GatewayController {
   constructor(private readonly companiesService: CompaniesService) {
-    super("companies");
+    super('companies');
   }
+
+  getCompany: RequestHandler = (req, res) =>
+    ctrlWrapper(this.getIdentifier('getCompany'), res, async () => {
+      const { companyId } = req.params;
+      if (!companyId) {
+        throw new HttpException(400, 'companyId is required to get company');
+      }
+      const getCompanyResult = await this.companiesService.getCompany(
+        companyId as string
+      );
+      if (getCompanyResult.isErr()) {
+        throw new HttpException(400, getCompanyResult.error.message);
+      }
+      return {
+        status: 200,
+        success: true,
+        data: getCompanyResult.value.data
+      } satisfies BaseResponse;
+    });
 }
--- apps/store-gateway/src/lib/router.ts
+++ apps/store-gateway/src/lib/router.ts
@@ -1,17 +1,19 @@
-import { apiConfig } from "../config/api.config";
-import express, { Express } from "express";
+import { apiConfig } from '../config/api.config';
+import express, { Express } from 'express';
 import {
   authRouter,
   companiesRouter,
-  emailValidationRouter,
-} from "../dependency-injection";
+  // companyCustomersRouter,
+  emailValidationRouter
+} from '../dependency-injection';
 
 export const createApiRouter = (app: Express) => {
   const baseApiRouter = express.Router();
 
-  baseApiRouter.use("/auth", [], authRouter);
-  baseApiRouter.use("/companies", [], companiesRouter);
-  baseApiRouter.use("/email-validation", [], emailValidationRouter);
+  baseApiRouter.use('/auth', [], authRouter);
+  baseApiRouter.use('/companies', [], companiesRouter);
+  baseApiRouter.use('/email-validation', [], emailValidationRouter);
+  // baseApiRouter.use('/company-customers', [], companyCustomersRouter);
 
   app.use(apiConfig.apiPath, baseApiRouter);
 };
